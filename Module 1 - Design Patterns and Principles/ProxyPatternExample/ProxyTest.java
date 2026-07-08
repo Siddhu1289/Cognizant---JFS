e5c77d97876dd5833aa349fba5d0fed3589048ff
Siddhu1289 <siddhugunda1919@gmail.com>
2026-07-08 19:49:37 +0530

    Added Proxy Pattern implementation

---
+++ Module 1 - Design Patterns and Principles/ProxyPatternExample/ProxyTest.java
@@ -0,0 +1,17 @@
+public class ProxyTest {
+
+    public static void main(String[] args) {
+
+        Image image = new ProxyImage("Nature.jpg");
+
+        System.out.println("First Call:");
+        image.display();
+
+        System.out.println();
+
+        System.out.println("Second Call:");
+        image.display();
+
+    }
+
+}
