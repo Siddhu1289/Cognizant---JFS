e6e277efa047f227b24a858662c17a65bbaa0932
Siddhu1289 <siddhugunda1919@gmail.com>
2026-07-08 18:53:43 +0530

    Added Decorator Pattern implementation

---
+++ Module 1 - Design Patterns and Principles/DecoratorPattern/DecoratorTest.java
@@ -0,0 +1,13 @@
+public class DecoratorTest {
+
+    public static void main(String[] args) {
+
+        Notifier notifier = new EmailNotifier();
+
+        notifier = new SMSNotifierDecorator(notifier);
+        notifier = new SlackNotifierDecorator(notifier);
+
+        notifier.send("Your order has been shipped!");
+
+    }
+}
